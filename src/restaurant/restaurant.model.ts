91d73071e89c4e25830499dec34dc8a5caa5b5c1
Victor Augusto <victor.silva452@outlook.com>
2019-06-29 14:39:03 -0300

    Model Restaurant finished

--- src/restaurant/restaurant.model.ts
+++ src/restaurant/restaurant.model.ts
@@ -1,4 +1,5 @@
 import * as mongoose from "mongoose"
+import { MongoClient, Mongos } from "mongodb";
 
 export interface MenuItem extends mongoose.Document{
 	name: string,
@@ -9,3 +10,29 @@ export interface Restaurant extends mongoose.Document{
 	name: string,
 	menu: MenuItem[]	
 }
+
+const menuScheme = new mongoose.Schema({
+	name:{
+		type: String,
+		required: true
+	}, 
+	price:{
+		type: Number,
+		required: true
+	}
+})
+
+const restaurantSchema = new mongoose.Schema({
+	neme: {
+		type: String,
+		menu:{
+			type: [menuScheme],
+			required: false,
+			select: false, //Não irá trazer o menu quando selecionado o restaurant
+			default: [] //Valor default no mongoose
+		}
+	}
+})
+
+//export const Menu = mongoose.model<MenuItem>("Menu", menuScheme)
+export const Restaurant = mongoose.model<Restaurant>("Restaurant", restaurantSchema)
