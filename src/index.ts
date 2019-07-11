465aa4be339db3ea89896c60e820365edf370d58
Victor Augusto <victor.silva452@outlook.com>
2019-07-10 23:06:38 -0300

    restaurantscommits

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Server } from "./server/server"
 import { userRouter } from "./users/users.router"
+import { restaurantsRouter } from "./restaurant/restaurant.router"
 
 const server = new Server()
-    server.bootstrap([userRouter]).then(server => {
+    server.bootstrap([userRouter, restaurantsRouter]).then(server => {
         console.log("Server Running on ", server.application.address());
     })
     .catch(error => {
