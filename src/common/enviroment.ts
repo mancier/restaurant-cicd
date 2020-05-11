776b6f2524de6503d97b73355e8fffef30f04c1b
Victor Augusto <vitor.silva@ikatec.com.br>
2020-05-11 20:44:45 -0300

    Modifications in DNS

--- src/common/enviroment.ts
+++ src/common/enviroment.ts
@@ -2,7 +2,7 @@ export const enviroment = {
     server: {
         port: process.env.SERVER_PORT || 3000,
         db:{
-            url: process.env.DB_URL || "mongodb://localhost:27017/",
+            url: process.env.DB_URL || "mongodb://db:27017/",
             user: process.env.DB_USER || "admin",
             schema: process.env.DB_SCHEMA || "meat-api",
             password: process.env.DB_PASSWORD || "K1WaMs04sbnNV6sb1ZE7RPTlOm7V4kG",
